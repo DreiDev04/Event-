2b3b17b1cb26175e3e9d7b61f20cb1e3c4c47257
John Andrei <tacujan.andre@gmail.com>
2024-07-26 02:01:42 +0800

    added: join/create

--- app/home/joincreate/page.tsx
+++ app/home/joincreate/page.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const JoinCreate = () => {
   return (
-    <div>JoinCreate</div>
-  )
-}
+    <div className="flex flex-col gap-10 ">
+      <div className="flex flex-col gap-5">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Join
+        </h1>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+          Enter the code provided by the group admin to join the group. You can
+          also create a new group below.
+        </p>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="text" placeholder="enter code" />
+          <Button type="submit">Join</Button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Create Group
+        </h1>
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+          Create a new group and invite your friends to join. You will be the
+          admin of the group.
+        </p>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Button>Create</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default JoinCreate
+export default JoinCreate;
