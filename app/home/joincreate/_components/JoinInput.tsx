5921a6c502bf510647167f42e046ef58a9f359c1
John Andrei <tacujan.andre@gmail.com>
2024-08-10 00:04:25 +0800

    update: make join a form

--- app/home/joincreate/_components/JoinInput.tsx
+++ app/home/joincreate/_components/JoinInput.tsx
@@ -1,25 +1,34 @@
-"use client"
-import React,{useState} from 'react'
+"use client";
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 
 const JoinInput = () => {
-  const [values, setValues] = useState<string>();
+  const router = useRouter();
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const input = form.elements.namedItem("code") as HTMLInputElement;
+    const value = (input.value).trim();
+    // console.log(value);
+    router.push(`/home/joincreate/join/${value}`);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValues(e.target.value)
-  }
+  };
 
   return (
-    <>
-    <Input type="text" placeholder="enter code" onChange={handleChange} />
-    <Button asChild>
-      <Link href={`/home/joincreate/join/${values}`}>Join</Link>
-    </Button>
-    </>
-  )
-}
+    <form onSubmit={onSubmit} className="flex gap-4">
+      <Input
+        type="text"
+        name="code"
+        autoComplete="off"
+        required
+        placeholder="enter code"
+      />
+      <Button type="submit">Join</Button>
+    </form>
+  );
+};
 
-export default JoinInput
+export default JoinInput;
